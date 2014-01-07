62998e9552d95abf6b1bc1a0c97b2c270a49ee8a
Maxime <maxime.lafarie@gmail.com>
2014-01-07 20:27:56 +0100

    Update affichebase.sql

--- affichebase.sql
+++ affichebase.sql
@@ -1,9 +1,33 @@
 -- ==========================
 -- fichier : viderbase.sql
 -- base : BILLETERIE
--- auteur(s) : Nicolas DOUSSON
 -- auteur(s) : Maxime LAFARIE
+-- auteur(s) : Nicolas DOUSSON
 -- date : 07/01/14
--- role : 
--- projet : 
+-- role : Afficher le contenu des differentes tables de la base
+-- projet : Billeterie
 -- ==========================
+
+prompt Contenu de la table client :
+
+SELECT * FROM CLIENT;
+
+prompt Contenu de la table billet :
+
+SELECT * FROM BILLET;
+
+prompt Contenu de la table evenement :
+
+SELECT * FROM EVENEMENT;
+
+prompt Contenu de la table salle :
+
+SELECT * FROM SALLE;
+
+prompt Contenu de la table tarif :
+
+SELECT * FROM TARIF;
+
+prompt Contenu de la table siteresa :
+
+SELECT * FROM SITERESA;
