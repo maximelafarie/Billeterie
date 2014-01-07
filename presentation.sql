1abd2149311070c25bd6937237f0adfde59dacaa
Maxime <maxime.lafarie@gmail.com>
2014-01-07 21:10:22 +0100

    Update presentation.sql

--- presentation.sql
+++ presentation.sql
@@ -1,9 +1,14 @@
 -- ==========================
 -- fichier : viderbase.sql
 -- base : BILLETERIE
--- auteur(s) : Nicolas DOUSSON
 -- auteur(s) : Maxime LAFARIE
+-- auteur(s) : Nicolas DOUSSON
 -- date : 07/01/14
--- role : 
--- projet : 
+-- role : Ameliorer l'affichage des tables
+-- projet : Billeterie
 -- ==========================
+
+COLUMN nomcli FORMAT A15;
+COLUMN prenomcli FORMAT A15;
+COLUMN mailcli FORMAT A20;
+COLUMN adrcli FORMAT A15;
