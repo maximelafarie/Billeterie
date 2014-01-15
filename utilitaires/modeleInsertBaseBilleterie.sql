61c9ea2afd33dcbe9c712020e2004749f876ca70
Nicolas <nicolas.dousson.86@gmail.com>
2014-01-15 10:41:20 +0100

    Update modeleInsertBaseBilleterie.sql

--- utilitaires/modeleInsertBaseBilleterie.sql
+++ utilitaires/modeleInsertBaseBilleterie.sql
@@ -10,7 +10,7 @@
 
 -- INSERTION D'UN NOUVEAU CLIENT
 INSERT INTO client (idcli, nomcli, prenomcli, telcli, mailcli, adrcli)
-VALUES (1, 'PEST', 'BUDA', '0505050505', 'budapest@exemple.org', 'BUDAPEST');
+VALUES (1, 'MARTIN', 'Jean', '0505050505', 'martin@gmail.com', 'La Rochelle');
 
 -- INSERTION D'UN NOUVEAU TARIF
 INSERT INTO tarif (idtarif, prix, typetarif)
@@ -18,40 +18,41 @@ VALUES (1, 100, 'etudiant');
 
 -- INSERTION D'UN NOUVEAU SITE DE RESERVATION
 INSERT INTO siteresa (idsiteresa, nomsiteresa, urlsiteresa)
-VALUES (1, 'LACOMMANDE', 'www.lacommande.com');
+VALUES (1, 'LeMoinsCher', 'www.lmc.com');
 
 -- INSERTION D'UN NOUVEAU BILLET : Cas site resa
 INSERT INTO client (idcli, nomcli, prenomcli, telcli, mailcli, adrcli)
-VALUES (1, 'PEST', 'BUDA', '0505050505', 'budapest@exemple.org', 'BUDAPEST');
+VALUES (1, 'MARTIN', 'Jean', '0505050505', 'martin@gmail.com', 'La Rochelle');
 INSERT INTO tarif (idtarif, prix, typetarif)
-VALUES (1, 100, 'etudiant');
+VALUES (1, 100, 'plein');
 INSERT INTO siteresa (idsiteresa, nomsiteresa, urlsiteresa)
-VALUES (1, 'LACOMMANDE', 'www.lacommande.com');
+VALUES (1, 'LeMoinsCher', 'www.lmc.com');
 INSERT INTO salle (idsalle, nomsalle, adrsalle)
-VALUES (1, 'DUCHAMP', 'BURKINA');
+VALUES (1, 'CARLINE', 'La Rochelle');
 INSERT INTO evenement (idevt, titreevt, dateevt, typeevt, idsalle)
-VALUES (1, 'BOURRAGE', '01/02/2014', 'enfant', 1);
+VALUES (1, 'Les Joyaux', '01/02/2014', 'sport', 1);
 INSERT INTO billet (idbillet, codebillet, idcli, idtarif, idsiteresa, idevt)
-VALUES (1, '1055d255e', 1, 1, 1, 1);
+VALUES (1, '15ED68EP10EPD', 1, 1, 1, 1);
 
 -- INSERTION D'UN NOUVEAU BILLET : Cas vente direct particulier
 INSERT INTO client (idcli, nomcli, prenomcli, telcli, mailcli, adrcli)
-VALUES (1, 'PEST', 'BUDA', '0505050505', 'budapest@exemple.org', 'BUDAPEST');
+VALUES (1, 'MARTIN', 'Jean', '0505050505', 'martin@gmail.com', 'La Rochelle');
 INSERT INTO tarif (idtarif, prix, typetarif)
-VALUES (1, 100, 'etudiant');
+VALUES (3, 85, 'plein');
 INSERT INTO salle (idsalle, nomsalle, adrsalle)
-VALUES (1, 'DUCHAMP', 'BURKINA');
+VALUES (2, 'Melrane', 'Bordeaux');
 INSERT INTO evenement (idevt, titreevt, dateevt, typeevt, idsalle)
-VALUES (1, 'BOURRAGE', '01/02/2014', 'enfant', 1);
+VALUES (2, 'Clipto', '10/04/2014', 'concert', 2);
 INSERT INTO billet (idbillet, codebillet, idcli, idtarif, idsiteresa, idevt)
-VALUES (1, '1055d255e', 1, 1, NULL, 1);
+VALUES (3, '85PO36DM19CBD', 1, 1, NULL, 2);
+
 
 -- INSERTION D'UNE NOUVELLE SALLE
 INSERT INTO salle (idsalle, nomsalle, adrsalle)
-VALUES (1, 'DUCHAMP', 'BURKINA');
+VALUES (2, 'Melrane', 'Bordeaux');
 
 -- INSERTION D'UN NOUVEL EVENEMENT
 INSERT INTO salle (idsalle, nomsalle, adrsalle)
-VALUES (1, 'DUCHAMP', 'BURKINA');
+VALUES (2, 'Melrane', 'Bordeaux');
 INSERT INTO evenement (idevt, titreevt, dateevt, typeevt, idsalle)
-VALUES (1, 'BOURRAGE', '01/02/2014', 'enfant', 1);
+VALUES (2, 'Clipto', '10/04/2014', 'concert', 2);
